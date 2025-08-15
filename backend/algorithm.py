121da970e2078540e999e0e2810d440d381da3af
Aaron D'Mello <aaron.r.dmello@gmail.com>
2025-08-16 00:40:44 +1000

    added the main function for the algorithm

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -1,1 +1,22 @@
-print("hello world")
+"""
+In this file, we are implementing the timetabling algorithm for UQCourseCraft.
+
+Inputs to algorithm:
+User Data: A dictionary mapping days of the week to a list of time slots, indexable by military time (arr[0] represents 00:00-00:59).
+Course Data: A dictionary mapping course codes to a list of class types, each containing a list of time slots.
+
+Output: A dictionary mapping the course code and class type to the ideal preferences.
+"""
+
+def solve_timetable(time_slots, course_data) -> dict:
+    """
+    Solve the timetabling problem by finding the best fit for course classes into user preferences.
+    
+    Args:
+        time_slots (dict): A dictionary mapping days of the week to a list of time slots.
+        course_data (dict): A dictionary mapping course codes to a list of class types, each containing a list of time slots.
+
+    Returns:
+        dict: A dictionary mapping course code and class type to the ideal preferences.
+    """
+    return {}
