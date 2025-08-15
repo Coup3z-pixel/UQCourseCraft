ab63627651360c589fbf55413463a6f13076d7cf
Coup3z-pixel <seanmclennanqa@gmail.com>
2025-08-15 20:30:44 +1000

    set up

---
+++ session.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Set Session Name
+SESSION="UQCourseCraft"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+
+    # setup Writing window
+    tmux rename-window -t 0 'Writing'
+    tmux send-keys -t 'Writing' "nvim ." C-m
+
+    # Name first Pane and start zsh
+    tmux new-window -t $SESSION:1 -n 'frontend'
+	tmux send-keys -t 'frontend' 'cd ./frontend' C-m
+    tmux send-keys -t 'frontend' 'pnpm run start' C-m 'clear' C-m # Switch to bind script?
+
+	tmux new-window -t $SESSION:2 -n 'flask'
+	tmux send-keys -t 'flask' 'cd ./backend' C-m
+	tmux send-keys -t 'flask' 'source .venv/bin/activate' C-m
+	tmux send-keys -t 'flask' 'flask --app flaskr run --debug' C-m 'clear' C-m
+
+    # Setup an additional shell
+    tmux new-window -t $SESSION:3 -n 'Shell'
+fi
+
+# Attach Session, on the Main window
+tmux attach-session -t $SESSION:0
