c672e39eb7e87e98ca37416bc23363f9f5ede16c
Coup3z-pixel <seanmclennanqa@gmail.com>
2025-08-16 16:34:23 +1000

    resolved conflict

--- backend/conversion.py
+++ backend/conversion.py
@@ -1,45 +1,48 @@
 from classes import *
+from main import *
 from constants import *
 
-def convertForAlgorithm(courses_activities: list) -> list[Class]:
+def convertForAlgorithm(courses_activities: list):
     classes = []
     time_slots = dict()
-
+    
     # Creates the Class instances, with the Time instances inside
     for course in courses_activities:
+        print(course["activity_code"])
         # Create time class
         time = Time(
             convertActivityNumber(course["activity_code"]),
-            JSON_TO_DAY.get(course["day"]),
+            course["day"],
             convertTime(course["start"]),
             convertMinToHours(course["duration"]),
-            0)
+            50)
         
         # if existing class exists, add time to it
         foundClassInstance = False
         for classInstance in classes:
-            if classInstance.course_code == course["course_code"] and classInstance.subclass_type == course["class_type"]:
+            if classInstance.course_code == course["course_code"] and classInstance.class_type == course["class_type"]:
                 classInstance.add_time(time)
                 foundClassInstance = True
                 break
         
         # if not, create new class and append to classes
         if (foundClassInstance == False):
-            times = [time]
-            classes.append(Class(course["course_code"], getClassType(course["class_type"]), course["class_type"], times))
-            
-    return classes 
-            
+            classes.append(Class(
+                course["course_code"], 
+                course["class_type"]))
+        
 
 def convertTime(time: str) -> float:
     hours, minutes = time.split(":")
     return int(hours) + (int(minutes) / 60)
 
-def convertActivityNumber(activityNum: str) -> int: 
-    return int(activityNum[:2])
+def convertActivityNumber(activityNum: str) -> int:
+    if "-" in activityNum: 
+        num, _ = activityNum.split("-")
+    else:
+        num = activityNum
+
+    return int(num)
 
 def convertMinToHours(duration: str) -> float:
     return int(duration) / 60
-
-def getClassType(subclass_type: str) -> str:
-    return subclass_type[0:len(subclass_type) - 1]
